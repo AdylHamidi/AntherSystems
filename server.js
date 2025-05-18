d2ec6161640cebcc2f9482f6d0606d5a3915db8a
Adil Hamidi <adil.hamidii123@gmail.com>
2025-05-18 08:25:43 +0200

    remove all comments from code

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-// server.js
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
@@ -9,37 +8,29 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
-// Serve static files from the 'public' folder
 app.use(express.static(path.join(__dirname, 'public')));
 
-// A waiting list for unpaired users
 const waitingUsers = [];
 
 io.on('connection', (socket) => {
-  // Determine the user's IP address and country
   const ip = socket.handshake.address;
   const geo = geoip.lookup(ip);
   socket.userCountry = geo ? geo.country : 'Unknown';
 
-  // Listen for the client to "join" with a specific profile
   socket.on('join', (profile) => {
-    // Attach the profile + country to this socket
     socket.profile = {
       ...profile,
       country: socket.userCountry,
     };
     console.log(`User joined: ${JSON.stringify(socket.profile)}`);
 
-    // Attempt to pair with someone waiting
     if (waitingUsers.length > 0) {
       const partnerSocket = waitingUsers.shift();
       const roomID = `room-${socket.id}-${partnerSocket.id}`;
 
-      // Join both sockets to the same room
       socket.join(roomID);
       partnerSocket.join(roomID);
 
-      // Notify both clients that they're matched
       socket.emit('matched', {
         room: roomID,
         partner: partnerSocket.profile,
@@ -49,12 +40,10 @@ io.on('connection', (socket) => {
         partner: socket.profile,
       });
     } else {
-      // If no one is waiting, this user goes in the queue
       waitingUsers.push(socket);
     }
   });
 
-  // Listen for chat messages and broadcast them to the room
   socket.on('message', (data) => {
     io.to(data.room).emit('message', {
       message: data.message,
@@ -62,13 +51,11 @@ io.on('connection', (socket) => {
     });
   });
 
-  // When a user disconnects, remove them from the waiting queue if present
   socket.on('disconnect', () => {
     const index = waitingUsers.indexOf(socket);
     if (index !== -1) {
       waitingUsers.splice(index, 1);
     }
-    // Optionally, you could inform the partner that this user disconnected
   });
 });
 
